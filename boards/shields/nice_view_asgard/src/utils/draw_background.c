ab038cf23cee297cf23ca8fd3c9419cdedbd3707
Legyldor <joffrey.davaic@gmail.com>
2025-12-04 22:02:09 -0500

    logo test

--- boards/shields/nice_view_asgard/src/utils/draw_background.c
+++ boards/shields/nice_view_asgard/src/utils/draw_background.c
@@ -31,101 +31,53 @@ void draw_background(lv_obj_t* canvas, unsigned index) {
     lv_draw_img_dsc_t img_dsc;
     lv_draw_img_dsc_init(&img_dsc);
 
-    switch (index % 25) {
+    switch ((index * 2) % 25) {
         case 0: {
             lv_canvas_draw_img(canvas, 0, 0, &logo_0, &img_dsc);
-            break;
-        }
-        case 1: {
-            lv_canvas_draw_img(canvas, 0, 0, &logo_1, &img_dsc);
             break;
         }
         case 2: {
             lv_canvas_draw_img(canvas, 0, 0, &logo_2, &img_dsc);
             break;
         }
-        case 3: {
-            lv_canvas_draw_img(canvas, 0, 0, &logo_3, &img_dsc);
-            break;
-        }
         case 4: {
             lv_canvas_draw_img(canvas, 0, 0, &logo_4, &img_dsc);
-            break;
-        }
-        case 5: {
-            lv_canvas_draw_img(canvas, 0, 0, &logo_5, &img_dsc);
             break;
         }
         case 6: {
             lv_canvas_draw_img(canvas, 0, 0, &logo_6, &img_dsc);
             break;
         }
-        case 7: {
-            lv_canvas_draw_img(canvas, 0, 0, &logo_7, &img_dsc);
-            break;
-        }
         case 8: {
             lv_canvas_draw_img(canvas, 0, 0, &logo_8, &img_dsc);
-            break;
-        }
-        case 9: {
-            lv_canvas_draw_img(canvas, 0, 0, &logo_9, &img_dsc);
             break;
         }
         case 10: {
             lv_canvas_draw_img(canvas, 0, 0, &logo_10, &img_dsc);
             break;
         }
-        case 11: {
-            lv_canvas_draw_img(canvas, 0, 0, &logo_11, &img_dsc);
-            break;
-        }
         case 12: {
             lv_canvas_draw_img(canvas, 0, 0, &logo_12, &img_dsc);
-            break;
-        }
-        case 13: {
-            lv_canvas_draw_img(canvas, 0, 0, &logo_13, &img_dsc);
             break;
         }
         case 14: {
             lv_canvas_draw_img(canvas, 0, 0, &logo_14, &img_dsc);
             break;
         }
-        case 15: {
-            lv_canvas_draw_img(canvas, 0, 0, &logo_15, &img_dsc);
-            break;
-        }
         case 16: {
             lv_canvas_draw_img(canvas, 0, 0, &logo_16, &img_dsc);
-            break;
-        }
-        case 17: {
-            lv_canvas_draw_img(canvas, 0, 0, &logo_17, &img_dsc);
             break;
         }
         case 18: {
             lv_canvas_draw_img(canvas, 0, 0, &logo_18, &img_dsc);
             break;
         }
-        case 19: {
-            lv_canvas_draw_img(canvas, 0, 0, &logo_19, &img_dsc);
-            break;
-        }
         case 20: {
             lv_canvas_draw_img(canvas, 0, 0, &logo_20, &img_dsc);
             break;
         }
-        case 21: {
-            lv_canvas_draw_img(canvas, 0, 0, &logo_21, &img_dsc);
-            break;
-        }
         case 22: {
             lv_canvas_draw_img(canvas, 0, 0, &logo_22, &img_dsc);
-            break;
-        }
-        case 23: {
-            lv_canvas_draw_img(canvas, 0, 0, &logo_23, &img_dsc);
             break;
         }
         case 24: {
